b85b60ef126b01d494b93474127d6baf81cfda0b
blue <samuelwalter2001@gmail.com>
2024-08-17 02:01:58 -0500

    added exceptions in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,37 @@
 <?php
 
+use App\Http\Middleware\ForceJsonResponseMiddleware;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api(prepend: [ForceJsonResponseMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (ValidationException $exception) {
+            return jsonResponse(
+                status: 422,
+                message: 'Validation errors',
+                errors: $exception->errors()
+            );
+        });
+
+        $exceptions->render(function (AuthenticationException $exception) {
+            return jsonResponse(status: 401, message: $exception->getMessage());
+        });
+
+        $exceptions->render(function (NotFoundHttpException $exception) {
+            return jsonResponse(status: 404, message: $exception->getMessage());
+        });
+
     })->create();
