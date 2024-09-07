152a283f878ce2f0e82a68eab95485555776a650
blue <samuelwalter2001@gmail.com>
2024-09-06 22:23:14 -0500

    chore: translate enum values to english

--- app/Enums/Schools.php
+++ app/Enums/Schools.php
@@ -4,19 +4,20 @@ namespace App\Enums;
 
 enum Schools: string
 {
-    case MINING_ENGINEERING = 'Ingeniería de Minas';
-    case SYSTEMS_AND_INFORMATICS_ENGINEERING = 'Ingeniería de Sistemas e Informatica';
-    case ENVIRONMENTAL_ENGINEERING = 'Ingeniería Ambiental';
-    case AGROINDUSTRIAL_ENGINEERING = 'Ingeniería Agroindustrial';
-    case FISHERY_ENGINEERING = 'Ingeniería Pesquera';
-    case CIVIL_ENGINEERING = 'Ingeniería Civil';
-    case LAW = 'Derecho';
-    case MEDICINE = 'Medicina';
-    case BUSINESS_ADMINISTRATION = 'Administración';
-    case ACCOUNTING = 'Contabilidad';
-    case PUBLIC_MANAGEMENT_AND_SOCIAL_DEVELOPMENT = 'Gestión Publica y Desarrollo Social';
+    case MINING_ENGINEERING = 'Mining Engineering';
+    case SYSTEMS_AND_INFORMATICS_ENGINEERING = 'Systems and Information Engineering';
+    case ENVIRONMENTAL_ENGINEERING = 'Environmental Engineering';
+    case AGROINDUSTRIAL_ENGINEERING = 'Agroindustrial Engineering';
+    case FISHERY_ENGINEERING = 'Fisher Engineering';
+    case CIVIL_ENGINEERING = 'Civil Engineering';
+    case LAW = 'Law';
+    case MEDICINE = 'Medicine';
+    case BUSINESS_ADMINISTRATION = 'Business Administration';
+    case ACCOUNTING = 'Accounting';
+    case PUBLIC_MANAGEMENT_AND_SOCIAL_DEVELOPMENT = 'Public Management and Social Development';
 
-    public static function getValues(): array {
-        return array_map(fn($enum) => $enum->value, self::cases());
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
     }
 }
